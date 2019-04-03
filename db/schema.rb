422a9eb6d23d8e940a3012ac6d5e04ec3ebdc285
juanesgh <jerodriguez@miuandes.cl>
2019-04-03 10:07:07 -0300

    User validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_29_134851) do
+ActiveRecord::Schema.define(version: 2019_04_03_121423) do
 
   create_table "event_venues", force: :cascade do |t|
     t.string "name"
@@ -70,6 +70,7 @@ ActiveRecord::Schema.define(version: 2019_03_29_134851) do
     t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "phone"
   end
 
 end
