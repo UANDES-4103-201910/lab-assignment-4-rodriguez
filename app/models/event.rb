ee70c467158021927394fcc671cd1c163842e8ba
juanesgh <jerodriguez@miuandes.cl>
2019-04-03 10:29:56 -0300

    Tickettype and events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,14 @@
 class Event < ApplicationRecord
   belongs_to :event_venue
   has_many :ticket_types
+  validates :creation
+
+
+  def creation_ok
+  	self.where("created_at > ?", Date.today)
+  end 
+  
+  def creation
+  	errors.add("The event cannot be today or in the past.") unless creation_ok
+  end
 end
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,4 +1,5 @@
 class Ticket < ApplicationRecord
   belongs_to :ticket_type
   belongs_to :order
+  validates :price, numericality: {only_integer: true, :greater_than => 0}
 end
