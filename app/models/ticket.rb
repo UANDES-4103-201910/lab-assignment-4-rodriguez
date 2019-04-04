daa2768f6d99e7be37a5ec663da27f630a80f32d
Cristian <cefunck@miuandes.cl>
2019-04-04 01:57:49 -0300

    finished lab

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,4 +1,22 @@
 class Ticket < ApplicationRecord
   belongs_to :ticket_type
   belongs_to :order
+  before_save :ticket_cannot_be_created_after_the_start_date_of_the_event, :ticket_cannot_be_bought_after_the_start_date_of_the_event
+
+  private
+  def ticket_cannot_be_created_after_the_start_date_of_the_event
+    if (Event.find_by id: self.ticket_type.event_id).start_date < DateTime.now
+      puts "A ticket cannot be created after the start date of the associated event"
+      throw :abort
+    end
+  end
+
+  private
+  def ticket_cannot_be_bought_after_the_start_date_of_the_event
+    if (Event.find_by id: self.ticket_type.event_id).start_date < self.order.created_at
+      puts "A ticket cannot be bought after the start date of the associated event"
+      throw :abort
+    end
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
   has_many :orders
   validates :name, :password, presence: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
-  #validates :phone, numericality: {only_integer: true, :greater_than_or_equal_to => 100000000, :less_than_or_equal_to => 999999999999}
-  validates :phone, numericality: { only_integer: true }, length: { minimum: 9, maximum: 12 }
-  validates :password, format: {with: /(?=.*\d)(?=.*[a-zA-Z]).{4,12}/}#{with: /[A-Za-z0-9]/}
+  validates :phone, numericality: { only_integer: true }, length: { in: 9..12 }
+  validates :password, length: {in: 8..12}, format: {with: /(?=.*\d)(?=.*[a-zA-Z])/}
 end
